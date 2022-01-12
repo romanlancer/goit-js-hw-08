14f2816623ca563e0edca658e07dd316ee5e9c8b
roman_lancer <roman.girich@gmail.com>
2022-01-12 18:04:52 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,20 +3,23 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    const currentTime = data.seconds;
-    const timePlayed = data.percent;
-    localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+player.on('play', onPlayTime);
+player.on('timeupdate', throttle(onSaveTime, 1000));
 
-    if (timePlayed === 0.99) {
-      player.off('timeupdate');
-      localStorage.removeItem(LOCALSTORAGE_KEY);
-    }
-  }, 1000),
-);
+function onSaveTime(e) {
+  const currentTime = e.seconds;
+  // const timePlayed = e.percent
+  localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+
+  if (currentTime === e.duration) {
+    player.off('timeupdate');
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  }
+}
+
+function onPlayTime() {
+  player.on('timeupdate', throttle(onSaveTime, 1000));
+}
 
 const playbackTime = localStorage.getItem(LOCALSTORAGE_KEY);
-
 player.setCurrentTime(playbackTime);
