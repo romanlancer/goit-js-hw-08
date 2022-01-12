1e8d05021b548fdbcf24a38f6ff35fc24974b8bd
romanlancer <roman.girich@gmail.com>
2022-01-12 22:11:02 +0000

    Deploying to gh-pages from @ romanlancer/goit-js-hw-08@6fcb710564abcfce91fad419ec2e131e1129fba2 🚀

--- 02-video.ca1a9438.js
+++ 02-video.ca1a9438.js
@@ -4,4 +4,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=t(require("lodash.throttle"));function t(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),r=new Vimeo.Player(o),a="videoplayer-current-time";function n(e){const t=e.seconds;localStorage.setItem(a,t),t===e.duration&&(r.off("timeupdate"),localStorage.removeItem(a))}function u(){r.on("timeupdate",(0,e.default)(n,1e3))}r.on("play",u),r.on("timeupdate",(0,e.default)(n,1e3));const l=localStorage.getItem(a);r.setCurrentTime(l);
 },{"lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.21522466.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.ca1a9438.js.map
