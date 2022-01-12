2ed03321eb402f9a8caaae87ba6758ab64c9618e
Roman Hyrych <88936471+romanlancer@users.noreply.github.com>
2022-01-12 03:36:40 +0200

    All 3 tasks done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,22 @@
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.on('timeupdate', function (data) {
-  const currentTime = data.seconds;
-  localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
-  console.log(currentTime);
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTime = data.seconds;
+    const timePlayed = data.percent;
+    localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+
+    if (timePlayed === 0.99) {
+      player.off('timeupdate');
+      localStorage.removeItem(LOCALSTORAGE_KEY);
+    }
+  }, 1000),
+);
 
 const playbackTime = localStorage.getItem(LOCALSTORAGE_KEY);
-console.log(playbackTime);
+
 player.setCurrentTime(playbackTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const commentEl = document.querySelector('.feedback-form textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formValuesStorage = {};
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+
+  formData.forEach((value, name) => {
+    if (value === '') {
+      alert('no empty fields allowed');
+    }
+    console.log('name:', name);
+    console.log('value:', value);
+  });
+  e.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    formValuesStorage[e.target.name] = e.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValuesStorage));
+  }, 500),
+);
+
+function fillComment() {
+  const savedFormData = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+  if (parsedFormData) {
+    inputEl.value = parsedFormData.email || '';
+    commentEl.value = parsedFormData.message;
+  }
+}
+fillComment();
