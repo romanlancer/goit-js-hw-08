4bf2c273fe6ed6c63f2dfd5baddf87946329c622
Roman Hyrych <88936471+romanlancer@users.noreply.github.com>
2022-01-31 20:34:01 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,8 +4,6 @@ const inputEl = document.querySelector('.feedback-form input');
 const commentEl = document.querySelector('.feedback-form textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-const formValuesStorage = {};
-
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInputs, 500));
 
@@ -21,11 +19,16 @@ function onFormSubmit(e) {
     console.log('name:', name);
     console.log('value:', value);
   });
+
   e.target.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
 function onFormInputs(e) {
+  const formValuesStorage = {
+    email: inputEl.value,
+    message: commentEl.value,
+  };
   formValuesStorage[e.target.name] = e.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValuesStorage));
 }
@@ -33,9 +36,11 @@ function onFormInputs(e) {
 function fillComment() {
   const savedFormData = localStorage.getItem(LOCALSTORAGE_KEY);
   const parsedFormData = JSON.parse(savedFormData);
-  if (parsedFormData) {
-    inputEl.value = parsedFormData.email || '';
-    commentEl.value = parsedFormData.message || '';
+
+  if (parsedFormData === null) {
+    return;
   }
+  inputEl.value = parsedFormData.email || '';
+  commentEl.value = parsedFormData.message || '';
 }
 fillComment();
